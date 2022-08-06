a9a7631d7d74aa25784bd5a2813a5c02ab02fb54
Jamim Mehdi <jamimmehdi@gmail.com>
2022-08-06 16:07:18 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/App.css';
 const App = () => {
+  const [currentTime, setCurrentTime] = useState();
+  const [intervalId, setIntervalId] = useState();
 
+  const getLocalTime = () => {
+    if (intervalId) clearInterval(intervalId);
+    const newIntervalId = setInterval(() => {
+      setCurrentTime(new Date().toLocaleString());
+    }, 1000);
+    setIntervalId(newIntervalId);
+  }
+
+  useEffect(() => {
+    getLocalTime();
+  }, [])
   return (
     <div id="main">
-      <div className="date-time"></div>
+      <div className="date-time">
+        <div className="date-time">
+          {currentTime}
+        </div>
+      </div>
     </div>
   )
 }
